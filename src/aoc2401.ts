2755e103e81303ec3bf6c7e974ff669a2aa4ec5c
jasonmuzzy <15488120+jasonmuzzy@users.noreply.github.com>
2024-11-08 19:17:21 -0800

    :tada: It's almost time!

---
+++ src/aoc2401.ts
@@ -0,0 +1,9 @@
+import { run } from 'aoc-copilot';
+
+async function solve(inputs: string[], part: number, test: boolean, additionalInfo?: { [key: string]: string }): Promise<number | string> {
+    let answer: number | string = 0;
+    throw new Error('Not implemented'); // <-- Replace with your solution (raising an exception forces printing the example input and answer to the console)
+    return answer;
+}
+
+run(__filename, solve, true);
