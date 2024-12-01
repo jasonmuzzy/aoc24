eee0bcb456a37cd1341e826068092e239cbd39ab
jasonmuzzy <15488120+jasonmuzzy@users.noreply.github.com>
2024-11-30 21:34:06 -0800

    Day 1

--- src/__template.ts
+++ src/__template.ts
@@ -1,8 +1,8 @@
-import { run } from 'aoc-copilot';
+import { NotImplemented, run } from 'aoc-copilot';
 
 async function solve(inputs: string[], part: number, test: boolean, additionalInfo?: { [key: string]: string }): Promise<number | string> {
     let answer: number | string = 0;
-    throw new Error('Not implemented'); // <-- Replace with your solution (raising an exception forces printing the example input and answer to the console)
+    throw new NotImplemented('Not implemented'); // <-- Replace with your solution (raising an exception forces printing the example input and answer to the console)
     return answer;
 }
 
