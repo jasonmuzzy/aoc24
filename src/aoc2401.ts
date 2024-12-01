16cc3054f90d949802765134dc59f5da223d7689
jasonmuzzy <jasonmuzzy@hotmail.com>
2024-11-30 21:34:06 -0800

    Day 1

--- src/aoc2401.ts
+++ src/aoc2401.ts
@@ -1,9 +1,18 @@
 import { run } from 'aoc-copilot';
 
+// -------Part 1--------   -------Part 2--------
+// Day       Time  Rank  Score       Time  Rank  Score
+//   1   00:07:01  3058      0   00:11:22  3167      0
+
 async function solve(inputs: string[], part: number, test: boolean, additionalInfo?: { [key: string]: string }): Promise<number | string> {
-    let answer: number | string = 0;
-    throw new Error('Not implemented'); // <-- Replace with your solution (raising an exception forces printing the example input and answer to the console)
+    let answer = 0;
+    const twolists = inputs.map(input => (input.match(/\d+/g) ?? []).map(Number));
+    const list1 = twolists.map(l => l[0]).sort((a, b) => a - b);
+    const list2 = twolists.map(l => l[1]).sort((a, b) => a - b);
+    answer = part === 1
+        ? list1.reduce((pv, cv, i) => pv + Math.abs(cv - list2[i]), 0)
+        : list1.reduce((pv, cv) => pv + cv * list2.filter(n2 => n2 === cv).length, 0);
     return answer;
 }
 
-run(__filename, solve, true);
+run(__filename, solve);
